7172ebdcb954c7595ad95b6617dd0aadf1a215aa
naveengithub14 <42571374+naveengithub14@users.noreply.github.com>
2018-09-07 15:43:22 +0530

    Update odd number.py

--- odd number.py
+++ odd number.py
@@ -2,5 +2,5 @@ lower_limit,upper_limit = map(int,input().split())
 lower_limit = int(lower_limit)
 upper_limit = int(upper_limit)
 for i in range(lower_limit+1,upper_limit+1):
-	if(i%2 != 0):
-		print(i)
+	if (i%2 != 0):
+		print(i,end=" ")
