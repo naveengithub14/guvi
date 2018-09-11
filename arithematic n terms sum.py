14bedd5ea1b8ef99ffbf329f2c9ab9da35cd0058
naveengithub14 <42571374+naveengithub14@users.noreply.github.com>
2018-09-11 10:54:12 +0530

    Update arithematic n terms sum.py

--- arithematic n terms sum.py
+++ arithematic n terms sum.py
@@ -1,13 +1,6 @@
-# your code goes here
 n=input()
 a=input()
 d=input()
-def ap(n,a,d):
-  b=0
-  i=0
-  while(i<n):
-    b=b+a
-    a=a+d
-    i=i+1
-  return b
-print(ap(n,a,d))
+e=a+d
+b=e*n
+print b
